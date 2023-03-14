76cf17422ed0a159adce03ada2edfa0460f02ffe
Norah-Tonny <norahtonny75@gmail.com>
2023-03-14 16:17:17 +0300

    Added cors options

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const express=require("express");
-const app=express();
-const dotenv=require("dotenv")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-const cors=require("cors")
-const port=process.env.PORT||5000
-const mpesaRoutes=require("./routes/mpesa")
+const cors = require("cors")
+const port = process.env.PORT || 5000
+const mpesaRoutes = require("./routes/mpesa")
 dotenv.config()
 const corsOption = {
-    
-    origin:"*"
+
+
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+
 }
 app.use(cors(corsOption))
 mongoose.connect(process.env.MONG0_URL)
-.then(()=>{
-    app.listen(port,()=>{
-        console.log(`Application is running on port ${port} and DB is connected`)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Application is running on port ${port} and DB is connected`)
+        })
     })
-})
-.catch((err)=>{
-    console.log(err)
-})
+    .catch((err) => {
+        console.log(err)
+    })
 app.use(express.json())
 app.use("/api", mpesaRoutes);
 
